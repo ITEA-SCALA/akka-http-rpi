3b5e33de787dffab9f71f89371f15ecf0e5d2b96
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2021-11-24 20:55:54 +0200

    #master

--- src/main/scala/com/example/Boot.scala
+++ src/main/scala/com/example/Boot.scala
@@ -11,25 +11,26 @@ import scala.concurrent.ExecutionContext
 
 object Boot extends App with Route {
 
-  implicit val system: ActorSystem = ActorSystem(actorSystemName)
+  implicit val system: ActorSystem             = ActorSystem(actorSystemName)
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val ec: ExecutionContext            = system.dispatcher
-
-  val log = LoggerFactory.getLogger(getClass)
 
   // start a new HTTP server on port 8082 with our route as the handler
   Http().bindAndHandle(
     route, appConfig.application.host, appConfig.application.port) // TODO:  val bindingFuture = Http().newServerAt("0.0.0.0", 8082).bind(route)
 
-  println(s"Server online at http://localhost:8082/\nPress RETURN to stop...")
+  log.debug(s"Server online at http://${appConfig.application.host}:${appConfig.application.port}/")
+  log.debug(s"Press RETURN to stop...")
 
 }
 
 trait Route {
-  val route =
-    path("") {
+  val log = LoggerFactory.getLogger(getClass)
+
+  val route = path("") {
       get {
         complete {
+          log.debug(s"Say hello to akka-http on Raspberry Pi")
           "Say hello to akka-http on Raspberry Pi"
         }
       }
